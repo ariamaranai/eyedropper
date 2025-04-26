447dee926632708a7d126ac1c0f3dec8b7bea792
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-26 19:12:11 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,4 +1,7 @@
-chrome.action.onClicked.addListener(tab =>
+chrome.alarms.onAlarm.addListener(() =>
+  chrome.action.setBadgeText({ text: "" })
+);
+chrome.action.onClicked.addListener(tab => (
   chrome.windows.create({
     url: "index.htm?" + tab.windowId,
     type: "popup",
@@ -6,5 +9,8 @@ chrome.action.onClicked.addListener(tab =>
     height: 1,
     left: 9999,
     top: 9999
-  }
+  }),
+  chrome.alarms.create({
+    delayInMinutes: 5
+  })
 ));
